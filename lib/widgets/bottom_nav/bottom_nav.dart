7dba3683a943993d620186bb86d99287a3759c24
parkyuhyeon <pyh1111802@naver.com>
2024-03-25 18:22:34 +0900

    ➕ :: 다시 시작...!

--- lib/widgets/bottom_nav/bottom_nav.dart
+++ lib/widgets/bottom_nav/bottom_nav.dart
@@ -1,48 +1,10 @@
 import 'package:flutter/material.dart';
 
-class BottomNav extends StatefulWidget {
+class BottomNav extends StatelessWidget {
   const BottomNav({super.key});
 
   @override
-  State<BottomNav> createState() => _BottomNavState();
-}
-
-class _BottomNavState extends State<BottomNav> {
-  int indexNum = 0;
-
-  @override
-  void initState() {
-    indexNum = 0;
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: indexNum,
-      type: BottomNavigationBarType.fixed,
-      selectedItemColor: const Color(0xff171717),
-      selectedFontSize: 12,
-      selectedLabelStyle: const TextStyle(
-        fontWeight: FontWeight.w500,
-      ),
-      unselectedItemColor: const Color(0xffb4b5b7),
-      unselectedFontSize: 12,
-      unselectedLabelStyle: const TextStyle(
-        fontWeight: FontWeight.w500,
-      ),
-      backgroundColor: const Color(0xfff8f8f9),
-      elevation: 0,
-      items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "홈"),
-        BottomNavigationBarItem(icon: Icon(Icons.star), label: "건강"),
-        BottomNavigationBarItem(icon: Icon(Icons.boy_outlined), label: "챗봇"),
-        BottomNavigationBarItem(icon: Icon(Icons.sunny), label: "마음"),
-        BottomNavigationBarItem(icon: Icon(Icons.person), label: "마이페이지")
-      ],
-      onTap: (value) => setState(
-        () => indexNum = value,
-      ),
-    );
+    return const Placeholder();
   }
 }
