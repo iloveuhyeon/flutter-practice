0da6f7d0d72b1e598c5fda462d3ebb5ec603e242
parkyuhyeon <pyh1111802@naver.com>
2024-03-30 23:46:43 +0900

    ➕ :: 버튼은 되지만 여전히 화면 이동 안됨

--- lib/views/main_page/main_page.dart
+++ lib/views/main_page/main_page.dart
@@ -6,9 +6,14 @@ import 'package:practice/views/my_page/my_page.dart';
 import 'package:practice/views/splash/splash_page.dart';
 import 'package:practice/widgets/bottom_nav/bottom_nav.dart';
 
-class MainPage extends StatelessWidget {
-  MainPage({super.key});
-  int pageindex = 0;
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
   final page = [
     const HomePage(),
     const MyPage(),
@@ -19,10 +24,12 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const bottomNavState = BottomNav;
+
     return Scaffold(
       appBar: AppBar(),
-      body: page[pageindex],
-      bottomNavigationBar: BottomNav(),
+      body: page[/*여기에 bottomnav에 있는 pageindex값이 들어가야함 */],
+      bottomNavigationBar: const BottomNav(),
     );
   }
 }
