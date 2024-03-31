03c5e92428737bc37292a3d838c3c3d7cff60350
parkyuhyeon <pyh1111802@naver.com>
2024-03-31 23:26:09 +0900

    ➕ :: pageindex와 page를 전역상태로 바꾸고 버튼 완성 했지만 화면 상태변환이 안됨

--- lib/views/main_page/main_page.dart
+++ lib/views/main_page/main_page.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:practice/views/health/health_page.dart';
-import 'package:practice/views/heart/heart_page.dart';
-import 'package:practice/views/home_page/home_page.dart';
-import 'package:practice/views/my_page/my_page.dart';
-import 'package:practice/views/splash/splash_page.dart';
+import 'package:practice/utils/utils.dart';
 import 'package:practice/widgets/bottom_nav/bottom_nav.dart';
 
 class MainPage extends StatefulWidget {
@@ -14,21 +10,11 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  final page = [
-    const HomePage(),
-    const MyPage(),
-    const SplashPage(),
-    const HeartPage(),
-    const HealthPage(),
-  ];
-
   @override
   Widget build(BuildContext context) {
-    const bottomNavState = BottomNav;
-
     return Scaffold(
       appBar: AppBar(),
-      body: page[/*여기에 bottomnav에 있는 pageindex값이 들어가야함 */],
+      body: page[pageindex],
       bottomNavigationBar: const BottomNav(),
     );
   }
