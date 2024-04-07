ffa08ddde9b573d06cf8aac1828882e0831342f8
parkyuhyeon <pyh1111802@naver.com>
2024-04-07 16:25:02 +0900

    ➕ :: (#7) health_date, profile 만듦, svg 사진 오류

--- lib/views/health/health_page.dart
+++ lib/views/health/health_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practice/widgets/health/health_date.dart';
 import 'package:practice/widgets/home/surround_market.dart';
 import 'package:practice/widgets/topbar/topbar.dart';
 import 'package:practice/widgets/topbar/topbardate.dart';
@@ -6,19 +7,8 @@ import 'package:practice/widgets/topbar/topbardate.dart';
 class HealthPage extends StatelessWidget {
   const HealthPage({super.key});
 
-  int getWeekOfMonth(int year, int month, int day) {
-    DateTime date = DateTime(year, month, day);
-
-    DateTime firstDayOfMonth = DateTime(year, month, 1);
-    int firstDayOfWeek = firstDayOfMonth.weekday;
-    int weekOfMonth = ((day + firstDayOfWeek - 1) / 7).ceil();
-
-    return weekOfMonth;
-  }
-
   @override
   Widget build(BuildContext context) {
-    DateTime date = DateTime.now();
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
@@ -28,17 +18,7 @@ class HealthPage extends StatelessWidget {
             children: [
               TopBar(text: "영양 관리"),
               TopBarDate(text: "이주영님의 영양 상태"),
-              Container(
-                child: Text(
-                  "${date.year}년 ${date.month}월 ${getWeekOfMonth(date.year, date.month, date.day)}주차 ",
-                  style: const TextStyle(
-                    fontFamily: 'Pretendard',
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600,
-                    color: Color.fromRGBO(90, 90, 92, 1),
-                  ),
-                ),
-              ),
+              const HealthDate(),
               const SizedBox(
                 height: 28,
               ),
--- lib/views/my_page/my_page.dart
+++ lib/views/my_page/my_page.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:practice/widgets/my_page/profile.dart';
+import 'package:practice/widgets/topbar/topbar.dart';
 
 class MyPage extends StatelessWidget {
   const MyPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("MyPage"),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TopBar(text: "마이페이지"),
+              const Profile(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
