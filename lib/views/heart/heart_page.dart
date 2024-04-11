65f336f426134c4f961e9a044b99c09cc8df4de1
parkyuhyeon <pyh1111802@naver.com>
2024-04-11 12:22:09 +0900

    ➕ :: (#7) heart_page 버튼 만듦

--- lib/views/heart/heart_page.dart
+++ lib/views/heart/heart_page.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:practice/utils/index.dart';
 import 'package:practice/widgets/topbar/topbar.dart';
 
-class HeartPage extends StatelessWidget {
+class HeartPage extends StatefulWidget {
   const HeartPage({super.key});
 
+  @override
+  State<HeartPage> createState() => _HeartPageState();
+}
+
+class _HeartPageState extends State<HeartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,17 +21,36 @@ class HeartPage extends StatelessWidget {
           child: Column(
             children: [
               TopBar(text: "당신의 마음이 항상 빛나길"),
-              Container(
-                height: 50,
-                padding: const EdgeInsets.symmetric(vertical: 8),
-                child: Row(
-                  children: [
-                    HeartButton(
-                      text: "마음",
-                    ),
-                  ],
-                ),
-              )
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  HeartButton(
+                    text: '마음',
+                    width: 28,
+                    index: 0,
+                    isSelected: heartButtonIndex == 0,
+                    onPressed: () {
+                      setState(() {
+                        heartButtonIndex = 0;
+                      });
+                    },
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  HeartButton(
+                    text: '고민 털어놓기',
+                    width: 87,
+                    index: 1,
+                    isSelected: heartButtonIndex == 1,
+                    onPressed: () {
+                      setState(() {
+                        heartButtonIndex = 1;
+                      });
+                    },
+                  )
+                ],
+              ),
             ],
           ),
         ),
@@ -35,25 +60,48 @@ class HeartPage extends StatelessWidget {
 }
 
 class HeartButton extends StatelessWidget {
-  HeartButton({super.key, required this.text});
+  const HeartButton({
+    super.key,
+    required this.text,
+    required this.width,
+    required this.index,
+    required this.isSelected,
+    required this.onPressed,
+  });
+
   final String text;
-  bool isSelect = false;
+  final double width;
+  final int index;
+  final bool isSelected;
+  final VoidCallback onPressed;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-      height: 34,
-      child: Column(
-        children: [
-          Text(text),
-          Container(
-            height: 3,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(999),
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: onPressed,
+          child: Text(
+            text,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+              color: isSelected
+                  ? const Color.fromRGBO(40, 112, 251, 1)
+                  : const Color.fromRGBO(61, 61, 62, 1),
             ),
           ),
-        ],
-      ),
+        ),
+        if (isSelected)
+          Container(
+            height: 3,
+            width: width,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(999),
+              color: const Color.fromRGBO(40, 112, 251, 1),
+            ),
+          ),
+      ],
     );
   }
 }
