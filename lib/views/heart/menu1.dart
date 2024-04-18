d588328d57965b796d5171d35097999c2a6a0ced
parkyuhyeon <pyh1111802@naver.com>
2024-04-18 17:41:57 +0900

    ➕ :: (#7) 아이콘 컴포넌트로 만듦, heartpage에 적용, icon 적당한거 없음

--- lib/views/heart/menu1.dart
+++ lib/views/heart/menu1.dart
@@ -6,10 +6,38 @@ class Menu1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        HeartIcon(),
+        Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          child: const Row(
+            children: [
+              HeartIcon(
+                  firstColor: Color.fromRGBO(132, 23, 22, 1),
+                  secondColor: Color.fromRGBO(218, 37, 36, 1),
+                  icon: Icons.heart_broken,
+                  text: "외로움"),
+              SizedBox(
+                width: 12,
+              ),
+              HeartIcon(
+                  firstColor: Color.fromRGBO(22, 62, 138, 1),
+                  secondColor: Color.fromRGBO(36, 102, 228, 1),
+                  icon: Icons.water_drop_rounded,
+                  text: "지침"),
+              SizedBox(
+                width: 12,
+              ),
+              HeartIcon(
+                  firstColor: Color.fromRGBO(133, 49, 13, 1),
+                  secondColor: Color.fromRGBO(220, 81, 22, 1),
+                  icon: Icons.badge_rounded,
+                  text: "상처"),
+            ],
+          ),
+        ),
       ],
     );
   }
